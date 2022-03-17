04670163e32de729ebdb139a8714dc15b3c5a44e
IsabelaSales <isa.sales10@hotmail.com>
2022-03-16 21:21:43 -0300

    Aprendendo Vue

---
+++ src/components/TesteEvento.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="Teste-Evento">
+        <h2>Teste Evento</h2>
+        <p @click="deixarMaiuscila">{{mensagem}}</p>
+    </div>
+</template>
+<script>
+export default {
+    nome: 'TesteEvento',
+    data() {
+        return{
+            mensagem: 'Allan o Gostoso'
+        }
+    },
+    methods: {
+        deixarMaiuscila(){
+            this.mensagem =this.mensagem.toUpperCase();
+        }
+    }
+
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/TesteIf.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="testando-if"> 
+        <h1 v-if="podeV">Teste1</h1>
+        <h1 v-else>Teste2</h1>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TesteIf',
+    data(){
+        return{
+            podeV: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>>
